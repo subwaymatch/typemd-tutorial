43247bdf4032efb9e0d199b7ad80986e0257be10
Ye Joo Park <subwaymatch@gmail.com>
2020-09-22 06:58:03 -0500

    Add firebase config via dotenv

--- src/lib/firebaseConfig.js
+++ src/lib/firebaseConfig.js
@@ -1,9 +1,19 @@
 export default {
-  apiKey: 'AIzaSyC6Xjs-_bbgbt4cnll68LdvqVGrBkMT8jY',
-  authDomain: 'typemd-e4e9d.firebaseapp.com',
-  databaseURL: 'https://typemd-e4e9d.firebaseio.com',
-  projectId: 'typemd-e4e9d',
-  storageBucket: 'typemd-e4e9d.appspot.com',
-  messagingSenderId: '995812586934',
-  appId: '1:995812586934:web:8a5ab72d4e6f33f494bd17',
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
+
+// export default {
+//   apiKey: process.env.,
+//   authDomain: process.env.,
+//   databaseURL: process.env.,
+//   projectId: process.env.,
+//   storageBucket: process.env.,
+//   messagingSenderId: process.env.,
+//   appId: process.env.,
+// };
